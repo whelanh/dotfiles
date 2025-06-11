8f42ce7b8ae5454b190077a11ab2eacc820a46bb
whelanh <brickhousedevelopers@gmail.com>
2025-06-11 05:05:52 -0400

    Initial commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+### bling.fish source start
+test -f /usr/share/ublue-os/bling/bling.fish && source /usr/share/ublue-os/bling/bling.fish
+### bling.fish source end
---
+++ dot_config/private_fish/functions/ld.fish
@@ -0,0 +1,4 @@
+function ld --wraps='eza --long  --all --sort modified --total-size' --description 'alias ld=eza --long  --all --sort modified --total-size'
+  eza --long  --all --sort modified --total-size $argv
+        
+end
